1403ef66be40b3613088b1d7f1a0c655d851a950
Mayukh Sinha <33156064+mayukhsinha11699@users.noreply.github.com>
2021-10-03 11:48:11 +0530

    Optimised code

--- String/(CODERFORCES) Edu-Div2#105 A. ABC String.cpp
+++ String/(CODERFORCES) Edu-Div2#105 A. ABC String.cpp
@@ -1,206 +1,31 @@
-https://codeforces.com/contest/1494/problem/A
-#include<bits/stdc++.h>
+//https://codeforces.com/contest/1494/problem/A
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-#define mod 1e9 + 7
 
-bool isBalanced1(string exp) 
-{ 
-   // return false;  
-
-    bool flag = true; 
-    bool flag2 = true; 
-    int count = 0; 
-  
-    for (int i = 0; exp[i] != '\0'; i++) { 
-  
-        if (exp[i] == 'A') { 
-            count++; 
-        } 
-        else { 
-            count--; 
-        } 
-        if (count < 0) { 
-            flag = false; 
-            break; 
-        } 
-    } 
-    if (count != 0) { 
-        flag = false; 
-    } 
-  
-   count = 0;
-     for (int i = 0; exp[i] != '\0'; i++) { 
-  
-        if (exp[i] == 'C' || exp[i] == 'B') { 
-            count++; 
-        } 
-        else { 
-            count--; 
-        } 
-        if (count < 0) { 
-            flag2 = false; 
-            break; 
-        } 
-    } 
-    if (count != 0) { 
-        flag2 = false; 
-    } 
-  
-    if (flag) return true;
-    if(flag2) return true;
-    return false;  
-} 
-
-bool isBalanced2(string exp) 
-{ 
-
-  //  return false;  
-
-    bool flag = true; 
-    bool flag2 = true; 
-
-    int count = 0; 
-  
-    for (int i = 0; exp[i] != '\0'; i++) { 
-  
-        if (exp[i] == 'B') { 
-            count++; 
-        } 
-        else { 
-            count--; 
-        } 
-        if (count < 0) { 
-            flag = false; 
-            break; 
-        } 
-    } 
-    if (count != 0) { 
-        flag = false; 
-    } 
-
-    count = 0;
-     for (int i = 0; exp[i] != '\0'; i++) { 
-  
-        if (exp[i] == 'C' || exp[i] == 'A') { 
-            count++; 
-        } 
-        else { 
-            count--; 
-        } 
-        if (count < 0) { 
-            flag2 = false; 
-            break; 
-        } 
-    } 
-    if (count != 0) { 
-        flag2 = false; 
-    } 
-  
-   if (flag) return true;
-    if(flag2) return true;
-    return false;  
-} 
-
-bool isBalanced3(string exp) 
-{ 
-
-   // return false;  
-
-    bool flag = true; 
-    bool flag2 = true; 
-
-    int count = 0; 
-  
-    for (int i = 0; exp[i] != '\0'; i++) { 
-  
-        if (exp[i] == 'C') { 
-            count++; 
-        } 
-        else { 
-            count--; 
-        } 
-        if (count < 0) { 
-            flag = false; 
-            break; 
-        } 
-    } 
-    if (count != 0) { 
-        flag = false; 
-    } 
-  
-   count = 0;
-     for (int i = 0; exp[i] != '\0'; i++) { 
-  
-        if (exp[i] == 'B' || exp[i] == 'A') { 
-            count++; 
-        } 
-        else { 
-            count--; 
-        } 
-        if (count < 0) { 
-            flag2 = false; 
-            break; 
-        } 
-    } 
-    if (count != 0) { 
-        flag2 = false; 
-    } 
-
-  
-   if (flag == true) return true;
-    if(flag2 == true) return true;
-    return false;  
-} 
-
-void solve(){
-
-    string a;
-    cin>>a;
-    int n = a.length();
-
-    int ca = 0,cb = 0,cc = 0;
-
-    for(int i=0; i<n; i++){
-        if(a[i] == 'A') ca++;
-        if(a[i] == 'B') cb++;
-        if(a[i] == 'C') cc++;
-    }
-
-    if(ca == (cb+cc) ){
-        if(isBalanced1(a)){
-            cout<<"YES"<<endl;
-            return;
-        }
-    } 
-
-    if(cb == (ca+cc) ){
-        if(isBalanced2(a)){
-            cout<<"YES"<<endl;
-            return;
-        }
-    } 
-
-    if(cc == (cb+ca) ){
-        if(isBalanced3(a)){
-            cout<<"YES"<<endl;
-            return;
-        }
-    } 
-
-    cout<<"NO"<<endl;
-    return;
-
+bool solve() {
+  string s;
+  cin >> s;
+  vector<int> d(3);
+  int x = s[0] - 'A';
+  int y = s.back() - 'A';
+  if (x == y)
+    return false;
+  d[x] = 1; d[y] = -1;
+  if (count(s.begin(), s.end(), 'A' + x) == s.length() / 2)
+    d[3 ^ x ^ y] = -1;
+  else
+    d[3 ^ x ^ y] = 1;
+  int bal = 0;
+  for (char c : s) {
+    bal += d[c - 'A'];
+    if (bal < 0) return false;
+  }
+  return bal == 0;
 }
 
-int main(){
-
-    int t;
-    cin>>t;
-    
-    while(t--){
-        solve();
-    }
-    
-    return 0;
+int main() {
+  int t; cin >> t;
+  while (t--) {
+    cout << (solve() ? "YES\n" : "NO\n");
+  }
 }
